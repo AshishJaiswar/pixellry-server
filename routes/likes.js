59660c93e1fbe477ccedc5046faea33b41167fc8
Ashish Jaiswar <Ajaiswar123456789@gmnail.com>
2022-02-13 16:41:39 +0530

    Collection and like features done

--- routes/likes.js
+++ routes/likes.js
@@ -24,5 +24,22 @@ app.post('/like', (req, res) => {
 	});
 })
 
+app.post('/unlike', (req, res) => {
+	let requestBody = req.body
+
+	let user_id = requestBody.user_id
+	let img_id = requestBody.img_id
+
+	let sql = `DELETE FROM LIKES WHERE IMG_ID = ${img_id} and USER_ID = ${user_id}`
+	database.query(sql, (err) => {
+        if (err) {
+            res.status(400).json({ message: err});
+            return;
+        }else{
+            res.status(200).send("Unliked");
+            return
+        }
+	});
+})
 
 module.exports = app;
